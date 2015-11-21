6b894c944f836e879adbea540f446960bf2ba1e3
Craig Mayhew <craig@mayhew.io>
2015-11-21 18:20:21 +0000

    Added ssh server to install command

--- create-ubuntu-node.sh
+++ create-ubuntu-node.sh
@@ -7,4 +7,4 @@ sudo apt-get install aptitude ubuntu-minimal
 sudo aptitude markauto '~i!~nubuntu-minimal'
 
 #linux, xorg for x windows system
-sudo apt-get install xorg openbox linux-image-generic 
+sudo apt-get install linux-image-generic openbox openssh-server xorg
